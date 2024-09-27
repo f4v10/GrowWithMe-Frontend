efe85a42f03bac1c3f0bd89ae8aab2a8ace126e9
GabrielRiveraOfficial <gabriverlay@gmail.com>
2024-09-27 17:31:55 -0500

    feat(sales): update sales service

--- src/app/sales/services/sales.service.ts
+++ src/app/sales/services/sales.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SalesOrderEntity } from '../model/sales-order.entity';
+import {environments} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
+  private baseUrl:string = `${environments.serverBasePath}`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createSalesOrder(salesOrder: { ruc: number, orderTimestamp: string, invoiceId: string }): Observable<SalesOrderEntity> {
+    return this.http.post<SalesOrderEntity>(this.baseUrl, salesOrder);
+  }
+
+  updateSalesOrder(id: number, salesOrder: { ruc: number, orderTimestamp: string, invoiceId: string }): Observable<SalesOrderEntity> {
+    return this.http.put<SalesOrderEntity>(`${this.baseUrl}/${id}`, salesOrder);
+  }
+
+  addFarmerProductToSalesOrder(salesOrderId: number, productId: number): Observable<SalesOrderEntity> {
+    const url = `${this.baseUrl}/${salesOrderId}/items/${productId}`;
+    return this.http.put<SalesOrderEntity>(url, null);
+  }
+  deleteSalesOrder(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+  getSalesOrders(): Observable<SalesOrderEntity[]> {
+    return this.http.get<SalesOrderEntity[]>(this.baseUrl);
+  }
+  getSalesOrder(id: number): Observable<SalesOrderEntity> {
+    return this.http.get<SalesOrderEntity>(`${this.baseUrl}/${id}`);
+  }
+
+
 }
