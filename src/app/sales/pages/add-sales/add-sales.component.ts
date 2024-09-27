185b71ce90cd2c028140b862961c4e1bf4fb8d84
GabrielRiveraOfficial <gabriverlay@gmail.com>
2024-09-27 17:43:05 -0500

    feat(sales): update add sales component

--- src/app/sales/pages/add-sales/add-sales.component.ts
+++ src/app/sales/pages/add-sales/add-sales.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { SalesService } from '../../services/sales.service';
+import { SalesOrderEntity } from '../../model/sales-order.entity';
 
 @Component({
   selector: 'app-add-sales',
-  standalone: true,
-  imports: [],
   templateUrl: './add-sales.component.html',
-  styleUrl: './add-sales.component.css'
+  styleUrls: ['./add-sales.component.css']
 })
 export class AddSalesComponent {
+  ruc = 0;
+  orderTimestamp = '';
+  invoiceId = '';
 
+  constructor(private salesService: SalesService) { }
+
+  addSalesOrder() {
+    const salesOrder = new SalesOrderEntity();
+    salesOrder.ruc = this.ruc;
+    salesOrder.orderTimestamp = this.orderTimestamp;
+    salesOrder.invoiceId = this.invoiceId;
+
+    this.salesService.createSalesOrder(salesOrder)
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+          console.error(salesOrder);
+          console.error(error);
+        }
+      );
+  }
 }
