e07d93114f71f1f5e229c000c48854674c156c23
GabrielRiveraOfficial <gabriverlay@gmail.com>
2024-09-27 17:33:12 -0500

    feat(sales): update farmer product service

--- src/app/sales/services/farmer-product.service.ts
+++ src/app/sales/services/farmer-product.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environments} from "../../../environments/environment";
+import { FarmerProductPriceEntity } from '../model/farmer-product.entity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FarmerProductService {
+  private baseUrl:string = `${environments.serverBasePath}`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getFarmerProductPrices(): Observable<FarmerProductPriceEntity[]> {
+    return this.http.get<FarmerProductPriceEntity[]>(this.baseUrl);
+  }
+
+  getFarmerProductPriceById(id: number): Observable<FarmerProductPriceEntity> {
+    return this.http.get<FarmerProductPriceEntity>(`${this.baseUrl}/${id}`);
+  }
+
+  createFarmerProductPrice(farmerProductPrice: FarmerProductPriceEntity) {
+    const body = {
+      productId: farmerProductPrice.productId,
+      money: farmerProductPrice.money,
+      amount: farmerProductPrice.amount,
+      start: farmerProductPrice.start,
+      end: farmerProductPrice.end,
+      quantityProduct: farmerProductPrice.quantityProduct
+    };
+    return this.http.post<FarmerProductPriceEntity>(this.baseUrl, body);
+  }
+
+  updateFarmerProductPrice(id: number, farmerProductPrice: FarmerProductPriceEntity): Observable<FarmerProductPriceEntity> {
+    return this.http.put<FarmerProductPriceEntity>(`${this.baseUrl}/${id}`, farmerProductPrice);
+  }
+
+  deleteFarmerProductPrice(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
