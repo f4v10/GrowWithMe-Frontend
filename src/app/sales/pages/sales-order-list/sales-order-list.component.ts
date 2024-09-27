16641960242561bd08b2378a742082f7cf5ecb3c
GabrielRiveraOfficial <gabriverlay@gmail.com>
2024-09-27 17:41:46 -0500

    feat(sales): update sales order list component

--- src/app/sales/pages/sales-order-list/sales-order-list.component.ts
+++ src/app/sales/pages/sales-order-list/sales-order-list.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SalesService } from '../../services/sales.service';
+import { SalesOrderEntity } from '../../model/sales-order.entity';
+import { FarmerProductService } from '../../services/farmer-product.service';
+import { FarmerProductPriceEntity } from '../../model/farmer-product.entity';
 
 @Component({
   selector: 'app-sales-order-list',
-  standalone: true,
-  imports: [],
   templateUrl: './sales-order-list.component.html',
   styleUrl: './sales-order-list.component.css'
 })
-export class SalesOrderListComponent {
+export class SalesOrderListComponent implements OnInit {
+  id: number=0;
+  salesOrder: SalesOrderEntity = new SalesOrderEntity();
+  farmerProducts: FarmerProductPriceEntity[] = [];
+  productId: number = 0;
+  constructor(
+    private route: ActivatedRoute,
+    private salesService: SalesService,
+    private farmerProductService: FarmerProductService
+  ) { }
 
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('Id');
+    this.id = idParam ? +idParam : 0; // Si idParam es null, se asignará 0 a this.id
+    this.getSalesOrder();
+    this.getFarmerProducts(); // Llama a la nueva función aquí
+  }
+
+  getFarmerProducts(): void {
+    this.farmerProductService.getFarmerProductPrices()
+      .subscribe(
+        products => {
+          this.farmerProducts = products;
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
+
+  getSalesOrder(): void {
+    this.salesService.getSalesOrder(this.id)
+      .subscribe(
+        order => {
+          this.salesOrder = order;
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
+  addProductToOrder(productId: number): void {
+    this.salesService.addFarmerProductToSalesOrder(this.id, productId)
+      .subscribe(
+        order => {
+          this.salesOrder = order;
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
 }
