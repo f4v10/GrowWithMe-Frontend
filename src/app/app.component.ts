da38bc5acbafbe2e53b5ad507a875e0174cbe263
GabrielRiveraOfficial <gabriverlay@gmail.com>
2024-09-27 22:49:07 -0500

    feat(sales): add angular material components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,34 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TranslateService} from "@ngx-translate/core";
 
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor, MatButton} from "@angular/material/button";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatInputModule} from "@angular/material/input";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    MatFormFieldModule, 
+    MatToolbar, 
+    MatAnchor, 
+    MatButton, 
+    MatCard, 
+    MatCardActions, 
+    MatCardContent, 
+    MatCardHeader, 
+    MatCardTitle, 
+    MatSnackBar, 
+    MatSnackBarModule, 
+    MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
