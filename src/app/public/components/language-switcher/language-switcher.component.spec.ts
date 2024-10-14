429c0f2944633cf2ab74f8c6233d0879a04a09be
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-10-14 17:14:24 -0500

    fix: deleting components out of the context.

--- src/app/public/components/language-switcher/language-switcher.component.spec.ts
+++ src/app/public/components/language-switcher/language-switcher.component.spec.ts
@@ -10,7 +10,7 @@ describe('LanguageSwitcherComponent', () => {
     await TestBed.configureTestingModule({
       imports: [LanguageSwitcherComponent]
     })
-      .compileComponents();
+    .compileComponents();
 
     fixture = TestBed.createComponent(LanguageSwitcherComponent);
     component = fixture.componentInstance;
@@ -21,4 +21,3 @@ describe('LanguageSwitcherComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-
