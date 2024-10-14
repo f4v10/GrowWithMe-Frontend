fdf4f5cdc5dbacc42d0dba1cf6933bde7b28d59f
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-10-14 18:06:54 -0500

    feat: added router outlet with routes for iam.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
+import {FarmerLoginComponent} from "./iam/pages/farmer/login/login.component";
+import {FarmerSignupComponent} from "./iam/pages/farmer/signup/signup.component";
+import {ConsultantLoginComponent} from "./iam/pages/consultant/login/login.component";
+import {ConsultantSignupComponent} from "./iam/pages/consultant/signup/signup.component";
 
 export const routes: Routes = [
-
+  {path: 'farmer-login', component: FarmerLoginComponent},
+  {path: 'farmer-signup', component: FarmerSignupComponent},
+  {path: 'consultant-login', component: ConsultantLoginComponent},
+  {path: 'consultant-signup', component: ConsultantSignupComponent}
 ];
