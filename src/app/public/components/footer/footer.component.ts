4f20a28d8331100582bd9d15272d7786508201e3
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-10-14 19:17:25 -0500

    feat: added i18n and logo.

---
+++ src/app/public/components/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faFacebook, faInstagram, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import {faX} from "@fortawesome/free-solid-svg-icons";
+import {TranslateModule} from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+    FaIconComponent,
+    TranslateModule
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  protected readonly faFacebook = faFacebook;
+  protected readonly faInstagram = faInstagram;
+  protected readonly faYoutube = faYoutube;
+  protected readonly faX = faX;
+}
