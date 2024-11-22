270d18059068a916998c999fa9f2d8f0515c2e0f
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-11-22 18:07:49 -0500

    fix: fixed endpoint with http client.

--- src/app/crops/services/crop.service.ts
+++ src/app/crops/services/crop.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import {BaseService} from "../../shared/services/base.service";
 import {Crop} from "../model/crop.entity";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CropService extends BaseService<Crop>{
 
-  constructor() {
-    super();
-    this.resourceEndPoint = '/crops';
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/crops';
   }
 }
