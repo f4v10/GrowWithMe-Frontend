7bc7064b585ec0b0a8ddb16736c130e3b9a09b8e
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-11-22 19:03:06 -0500

    fix: added dependency.

--- src/app/iam/pages/farmer/sign-up/sign-up.component.ts
+++ src/app/iam/pages/farmer/sign-up/sign-up.component.ts
@@ -6,7 +6,7 @@ import {AuthenticationFarmerService} from "../../../services/farmer/authenticati
 import {FarmerSignUpRequest} from "../../../model/farmer/farmer-sign-up.request";
 import {BaseFormComponent} from "../../../../shared/components/base-form/base-form.component";
 import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
-import {MatError, MatFormField} from "@angular/material/form-field";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {NgIf} from "@angular/common";
@@ -19,6 +19,7 @@ import {MatOption, MatSelect} from "@angular/material/select";
     FormsModule,
     RouterLink,
     TranslateModule,
+    MatLabel,
     MatCard,
     MatCardHeader,
     MatCardTitle,
